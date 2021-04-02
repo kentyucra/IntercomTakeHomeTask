4ebbb73b94053370d6cbf4e5498f5e5a5a9e78b8
mckkentmwk <mckkentmwk@gmail.com>
2021-04-02 22:23:12 +0100

    add test when the answer is not precise

--- test/IntercomTests.cpp
+++ test/IntercomTests.cpp
@@ -5,7 +5,24 @@
 
 #define epsilon 0.00000000001f
 
-TEST(CustomerModule, Distance) {
+TEST(CustomerModule, DistanceOutPrecision) {
+    //NOTE: Puno and Juliaca are places where I am from in Peru.
+    Customer Puno(1, -15.8402, -70.0219, "Puno");
+    Customer Juliaca(2, -15.4997, -70.1297, "Juliaca");
+
+    long double distanceFromJuliacaToPuno = Puno.distanceTo(Juliaca);
+
+    //reference from https://www.onlineconversion.com/map_greatcircle_distance.htm
+    long double distanceFromReference = 39.00;
+   
+    if (fabs(distanceFromReference - distanceFromJuliacaToPuno) < epsilon) { 
+	 ASSERT_TRUE(false);
+    } else { // It will not work because the precision is a lot bigger than epsilon
+	 ASSERT_TRUE(true);
+    }
+}
+
+TEST(CustomerModule, DistanceInPrecision) {
     //NOTE: Puno and Juliaca are places where I am from in Peru.
     Customer Puno(1, -15.8402, -70.0219, "Puno");
     Customer Juliaca(2, -15.4997, -70.1297, "Juliaca");
@@ -15,9 +32,9 @@ TEST(CustomerModule, Distance) {
     //reference from https://www.onlineconversion.com/map_greatcircle_distance.htm
     long double distanceFromReference = 39.58184734554036;
    
-    if (fabs(distanceFromReference - distanceFromJuliacaToPuno) < epsilon) { 
+    if (fabs(distanceFromReference - distanceFromJuliacaToPuno) < epsilon) {  // It should work, diff is within precision
 	 ASSERT_TRUE(true);
-    } else {
+    } else { 
 	 ASSERT_TRUE(false);
     }
 }
